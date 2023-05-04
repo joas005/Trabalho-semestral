87b87d270fc3a85ca5bc913639b23ebc58665ae4
Joas <joaoferreirapaula005@gmail.com>
2023-05-04 09:57:37 -0300

    Ultimas alterações do projeto

--- conversor.py
+++ conversor.py
@@ -17,19 +17,20 @@ while True:
                 os.system('cls' if os.name == 'nt' else 'clear')
                 base = input(
                     "Você deseja converter seu decimal para qual base?\n\n\033[35m[1] Binário.\n\033[34m[2] Hexadecimal.\n\033[36m[3] Octadecimal.\033[0m\n\n> ")
-                numInserido = int(
-                    input("Insira seu número decimal aqui: "))
-                numDecimal = numInserido
                 numConvertido = ''
 
                 match (base):
                     case '1':
+                        numInserido = int(input("Insira seu número decimal aqui: "))
+                        numDecimal = numInserido
                         sistema = '\033[35mbinário\033[0m'
                         while numDecimal > 0:
                             numConvertido += str(numDecimal % 2)
                             numDecimal = numDecimal // 2
 
                     case '2':
+                        numInserido = int(input("Insira seu número decimal aqui: "))
+                        numDecimal = numInserido
                         sistema = '\033[34mhexadecimal\033[0m'
                         hex = [0, 1, 2, 3, 4, 5, 6, 7, 8,
                                9, 'A', 'B', 'C', 'D', 'E', 'F']
@@ -38,6 +39,8 @@ while True:
                             numDecimal = numDecimal // 16
 
                     case '3':
+                        numInserido = int(input("Insira seu número decimal aqui: "))
+                        numDecimal = numInserido
                         sistema = '\033[36moctadecimal\033[0m'
                         while numDecimal > 0:
                             numConvertido += str(numDecimal % 8)
@@ -50,15 +53,16 @@ while True:
                         os.system('cls' if os.name == 'nt' else 'clear')
                         continue
 
-                if sistema == '\033[34mhexadecimal\033[0m' or sistema == '\033[36moctadecimal\033[0m':
-                    numInvertido = ''
-                    for digito in range(len(numConvertido)-1, -1, -1):
-                        numInvertido += str(numConvertido[digito])
-                    numConvertido = numInvertido
+                # invertendo os números
+                numInvertido = ''
+                for digito in range(len(numConvertido)-1, -1, -1):
+                    numInvertido += str(numConvertido[digito])
+                numConvertido = numInvertido
 
                 print(
                     f"\nO número \033[32m{numInserido}\033[0m dentro do sistema {sistema} é: {numConvertido}.")
-                time.sleep(5)
+                time.sleep(2)
+                input('Enter continua...')
                 os.system('cls' if os.name == 'nt' else 'clear')
                 break
 
